ea181b713efe485d4dcffbd2e6085158f054e9a1
petra <petra.spirkova@apiumhub.com>
2019-05-08 22:39:54 +0200

    add search form in the item list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,23 @@ import { ItemListComponent } from './containers/item-list/item-list.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './shared/material.module';
+// import { SearchFormComponent } from './components/search-form/search-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemListComponent,
-    ItemDetailComponent
+    ItemDetailComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   providers: [],
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 import {
   MatCardModule
 } from '@angular/material/card';
+import {
+  MatButtonModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule
+} from '@angular/material';
 
 
 @NgModule({
   imports: [
     MatCardModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule
   ],
   exports: [
     MatCardModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule
   ]
 })
 export class MaterialModule { }
